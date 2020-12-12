dc1321848937ce802f2fe3be3f80c0e5599c5da0
Andras Horvath <andrashor@gmail.com>
2020-12-13 00:55:53 +0100

    1-16 added

--- chapter-1/src/1-16longest-line.c
+++ chapter-1/src/1-16longest-line.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-#define MAXLENGTH 100
+#define MAXLENGTH 10
 
 int mygetline(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -11,15 +10,28 @@ int main()
     char currentline[MAXLENGTH];
     int length;
     int max;
+    int reallen;
+    int i;
 
+    reallen = 0;
     max = 0;
+    i = 0;
     while ((length = mygetline(currentline, MAXLENGTH)) > 0)
+    {   
+        reallen = reallen + length;
+        ++i; 
+        if (currentline[length-1] == '\n') {
+            i = 0;
+            printf("This is the correct length = %d\n", reallen-1);
+            reallen = 0;
+        }
         if (length > max) {
             max = length;
             copy(longestline, currentline);
         }
+    }    
     if (max > 0)
-        printf("%s", longestline);
+        printf("longest line: %s\n", longestline);
     return 0; 
 }
 
@@ -29,10 +41,10 @@ int mygetline(char s[], int limit)
 
     for (i=0; i<limit-1 && (c=getchar()) != EOF && c!='\n'; ++i)
         s[i] = c;
-    if (c=='\n') {
+    if (c == '\n') {
         s[i] = c;
         ++i;
-    }    
+    }   
     s[i] = '\0';
     return i;
 }
